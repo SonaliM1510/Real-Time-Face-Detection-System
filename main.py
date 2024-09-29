325697705aebf33da3a05a4124cf8e5687b44da1
Sonali Maithani <sonali.21gcebit132@galgotiacollege.edu>
2024-09-28 23:07:03 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+face_cap=cv2.CascadeClassifier('C:/Users/w10/AppData/Roaming/Python/Python312/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+if face_cap.empty():
+    print("Error loading cascade file. Check the file path and permissions.")
+
+video_cap=cv2.VideoCapture(0)
+while True:
+    ret, video_data=video_cap.read()
+    col=cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
+    faces=face_cap.detectMultiScale(
+        col,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+	)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(video_data,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.imshow("live_video",video_data)
+    if cv2.waitKey(10)==ord("a"):
+        break
+video_cap.release()
